a993e67db209ad6ef650dd7e97557ac742868a5a
Binbin Dong <binbin.dong1994@gmail.com>
2023-07-06 01:02:38 +0200

    updated var list

--- inputFiles/pkl_to_hdf5.py
+++ inputFiles/pkl_to_hdf5.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 import h5py
 
 with open('train_var_list.txt', 'r') as f1:
@@ -10,9 +11,7 @@ var_names = [line.strip() for line in lines]
 pkl_data = pd.read_pickle('df_test.pkl')
 input_data = pkl_data[var_names]
 labels = pkl_data['Sample']
-print(labels)
-Y_test = pd.get_dummies(labels).values
-print(Y_test)
+Y_test = np.eye(2,dtype=int)[labels]
 
 outputfile = h5py.File('df_test.h5','w')
 outputfile.create_dataset('X_test', data=input_data, compression='gzip')
