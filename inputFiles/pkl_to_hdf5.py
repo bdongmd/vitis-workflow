190aea37ab1cc897a7265ab3039d9f2f57daf204
Binbin Dong <binbin.dong1994@gmail.com>
2023-09-20 11:17:56 +0200

    fixed indentation

--- inputFiles/pkl_to_hdf5.py
+++ inputFiles/pkl_to_hdf5.py
@@ -15,14 +15,14 @@ var_names = [line.strip() for line in lines]
 pkl_data = pd.read_pickle('df_test.pkl')
 subset_data = pkl_data[var_names]
 labels = pkl_data['Sample']
-Y_test = np.eye(2,dtype=int32)[labels]
+Y_test = np.eye(2,dtype=np.int32)[labels]
 
 with h5py.File('subset_df_test.h5', 'w') as h5f:
-	for col_name, col_data in subset_data.items():
-		h5f.create_dataset(col_name, data=col_data)
-h5f.create_dataset('labels', data=labels)
-h5f.create_dataset('Y_test', data=Y_test)
-h5f.close()
+    for col_name, col_data in subset_data.items():
+        h5f.create_dataset(col_name, data=col_data)
+    h5f.create_dataset('labels', data=labels)
+    h5f.create_dataset('Y_test', data=Y_test)
+    h5f.close()
 
 #outputfile = h5py.File('df_test.h5','w')
 #outputfile.create_dataset('X_test', data=input_data, compression='gzip')
