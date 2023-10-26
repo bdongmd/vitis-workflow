8f17a2ddfb692b9ddfda1132bf7515066a8dcc05
Binbin Dong <binbin.dong1994@gmail.com>
2023-10-26 10:23:12 -0400

    moved few func to util

--- util.py
+++ util.py
@@ -1,15 +1,52 @@
+import os
+import json
+import timeit
 import h5py
 import numpy as np
+import pandas as pd
 from joblib import load
 
-def load_data(inputFile="inputFiles/df_test.h5", scaleFile="inputFiles/std_scaler.bin", featureType=np.float32, labelType=np.int64):
-	'''load testing dataset'''
-	h5f = h5py.File(inputFile, 'r')
-	scaler = load(scaleFile)
-	features = np.array(h5f['X_test'], dtype=featureType)
-	features = scaler.transform(features)
-	labels = np.array(h5f['Y_test'], dtype=labelType)
-	labels = np.argmax(labels, axis=-1)
-	h5f.close()
+def load_config(filename):
+	with open(filename, "r") as f:
+		return json.load(f)
 
-	return features, labels
+def print_model(model):
+	''' Print model structure '''
+	print("------------------------------")
+	for layer in model.layers:
+		print(f"Layer name: {layer.name}")
+		if layer.get_weights():
+			weights = layer.get_weights()[0]
+			print(f" - Weight Shape: {weights.shape}")
+			print(f" - Weight Type: {weights.dtype}")
+	print("------------------------------")
+
+def load_dataset(dataset, scaler, verbose):
+	with h5py.File(dataset, 'r') as h5f:
+		labels = np.array(h5f['labels']) if 'labels' in h5f else None
+		input_data = {name: np.array(data) for name, data in h5f.items() if name != 'labels'}
+	if verbose:
+		print("------------------------------")
+		print("Data reading from: ", dataset)
+		for col_name, data_array in input_data.items():
+			print(f"-- Feature: {col_name}, Data Type: {data_array.dtype}")
+	
+	scaler = load(scaler)
+	input_df = pd.DataFrame(input_data)
+	scaled_array = scaler.transform(input_df)
+	if verbose:
+		print("------------------------------")
+		print("After scaler:")
+		scaled_data = {col_name: scaled_array[:, idx] for idx, col_name in enumerate(input_df.columns)}
+		for col_name, data_array in scaled_data.items():
+			print(f"-- Feature: {col_name}, Data Type: {data_array.dtype}")
+
+	return labels, scaled_array
+
+def time_prediction(model, inputdata, labels):
+	start_time = timeit.default_timer()
+	model_score = model.predict(inputdata)
+	final_time = timeit.default_timer()
+	latency = final_time - start_time
+	throughput = len(labels) / latency
+	return latency, throughput, model_score
